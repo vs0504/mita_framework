7de72c5bc2c001d96961767400464b8df1c3b91c
Prathap <teja.kotapati@machint.com>
2023-01-31 19:03:08 +0530

    Adding files for cors issue

---
+++ server/src/main/java/com/testsigma/config/CorsFilter.java
@@ -0,0 +1,46 @@
+package com.testsigma.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+public class CorsFilter implements Filter {
+
+    private final Logger log = LoggerFactory.getLogger(CorsFilter.class);
+
+    public CorsFilter() {
+        log.info("SimpleCORSFilter init");
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        log.info("Inside Cors Filter");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
+
+
+
--- server/src/main/java/com/testsigma/config/WebConfigurer.java
+++ server/src/main/java/com/testsigma/config/WebConfigurer.java
@@ -11,11 +11,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@EnableWebMvc
 public class WebConfigurer implements WebMvcConfigurer {
 
   @Override
@@ -23,6 +26,10 @@ public class WebConfigurer implements WebMvcConfigurer {
     argumentResolvers.add(new SpecificationHandlerMethodArgumentResolver());
   }
 
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
     configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
